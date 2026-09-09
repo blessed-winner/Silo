a59cbe2d949c7194ba74b302f2b8ea36fc359489
Winner <blessedwinner66@gmail.com>
2026-09-09 14:07:38 +0200

    Initial commit

---
+++ src/test/java/org/xenon/silo/SiloApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xenon.silo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
