bc6ee32c0d07423330009a3fba64b5fa1faba48a
Winner <blessedwinner66@gmail.com>
2026-09-11 08:07:29 +0200

    Addition of security filter chain

---
+++ src/main/java/org/xenon/silo/archive/shared/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package org.xenon.silo.archive.shared.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final AppConfig appConfig;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config){
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        var provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(appConfig.passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http
+                .sessionManagement(c->c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        c -> c.requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
+                                                            .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
+                                                            .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
+                                                            .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+}
