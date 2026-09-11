18d39c063a4287d8d0f0d38b3683a42654b540a2
Winner <blessedwinner66@gmail.com>
2026-09-11 08:48:50 +0200

    Filter chaining implemented

--- src/main/java/org/xenon/silo/archive/shared/config/SecurityConfig.java
+++ src/main/java/org/xenon/silo/archive/shared/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -14,6 +15,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.xenon.silo.archive.shared.filters.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -44,7 +48,11 @@ public class SecurityConfig {
                                                             .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
                                                             .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
                                                             .anyRequest().authenticated()
-                );
+                ).addFilterBefore(appConfig.jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(c->{
+                    c.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+                    c.accessDeniedHandler((request,response,accessDeninedException) -> {response.setStatus(HttpStatus.FORBIDDEN.value());});
+                });
 
         return http.build();
     }
