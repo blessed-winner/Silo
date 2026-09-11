db5479d69e71a4c4cea9d51d481a7da5f437b018
Winner <blessedwinner66@gmail.com>
2026-09-11 07:06:12 +0200

    Addition of custom user details

---
+++ src/main/java/org/xenon/silo/archive/auth/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package org.xenon.silo.archive.auth.infrastructure.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+import org.xenon.silo.archive.users.domain.UserRepository;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email).orElseThrow(()->new UsernameNotFoundException("User not found"));
+        return new User(
+                user.getEmail(),
+                user.getPasswordHash(),
+                Collections.emptyList()
+        );
+    }
+}
