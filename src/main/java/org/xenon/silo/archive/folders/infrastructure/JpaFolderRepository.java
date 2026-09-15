6e4764b8854279a1c41e70f47a9dee9158022fcc
Winner <blessedwinner66@gmail.com>
2026-09-15 16:02:42 +0200

    Cleared unused imports and populated the jpa repository mapper with relevant functions

--- src/main/java/org/xenon/silo/archive/folders/infrastructure/JpaFolderRepository.java
+++ src/main/java/org/xenon/silo/archive/folders/infrastructure/JpaFolderRepository.java
@@ -2,7 +2,52 @@ package org.xenon.silo.archive.folders.infrastructure;
 
 
 import org.springframework.stereotype.Repository;
+import org.xenon.silo.archive.folders.domain.Folder;
+import org.xenon.silo.archive.folders.domain.FolderRepository;
+
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public class JpaFolderRepository {
+public class JpaFolderRepository implements FolderRepository {
+
+    private final FolderMapper folderMapper;
+    private final SpringDataJpaRepository springDataJpaRepository;
+
+    public JpaFolderRepository(FolderMapper folderMapper, SpringDataJpaRepository springDataJpaRepository){
+        this.folderMapper = folderMapper;
+        this.springDataJpaRepository = springDataJpaRepository
+    }
+
+    @Override
+    public Optional<Folder> findById(UUID id){
+       return springDataJpaRepository.findById(id).map(folderMapper::toDomain);
+    }
+
+    @Override
+    public Folder save(Folder folder){
+        FolderJpaEntity toSave = folderMapper.toEntity(folder);
+        springDataJpaRepository.save(toSave);
+        return folderMapper.toDomain(toSave);
+    }
+
+    @Override
+    public List<Folder> findAllByOwnerIdAndIsActiveTrue(UUID ownerId){
+        return springDataJpaRepository.findAllByOwnerIdAndIsActiveTrue(ownerId)
+                .stream()
+                .map(folderMapper::toDomain)
+                .toList();
+    }
+
+    @Override
+    public List<Folder> findAllByOwnerIdAndIsActiveTrueAndParentId(UUID ownerId, UUID parentId){
+        return springDataJpaRepository.findAllByOwnerIdAndIsActiveTrueAndParentId(ownerId, parentId)
+                .stream()
+                .map(folderMapper::toDomain)
+                .toList();
+    }
+
 }
--- src/main/java/org/xenon/silo/archive/folders/infrastructure/SpringDataJpaRepository.java
+++ src/main/java/org/xenon/silo/archive/folders/infrastructure/SpringDataJpaRepository.java
@@ -1,15 +1,13 @@
 package org.xenon.silo.archive.folders.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.xenon.silo.archive.folders.domain.FolderRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface SpringDataJpaRepository extends JpaRepository<FolderJpaEntity, UUID> {
 
     List<FolderJpaEntity> findAllByOwnerIdAndIsActiveTrue(UUID ownerId);
 
-    List<FolderRepository> findAllByOwnerIdAndIsActiveTrueAndParentId(UUID ownerId, UUID parentId);
+    List<FolderJpaEntity> findAllByOwnerIdAndIsActiveTrueAndParentId(UUID ownerId, UUID parentId);
 }
